3f901e9e5d5712afcdcdd13dcb26fbed943819ba
Dante <dalvarezt@unjbg.edu.pe>
2024-06-13 02:54:12 -0500

    Actualiza ejercicio 1 de estructuras repetitivas: Validar si el número ingresado es positivo

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -5,9 +5,13 @@ int main() {
 	int numeros, multiplos;
 	cout << "Ingrese una cantidad (positivos) que luego seran los primeros multiplos de 7: ";
 	cin >> numeros;
-	for ( int i = 1; i <= numeros; i++ ) {
-		multiplos = i * 7;
-		cout << multiplos << endl;
+	if ( numeros > 0 ) {
+		for ( int i = 1; i <= numeros; i++ ) {
+			multiplos = i * 7;
+			cout << multiplos << endl;
+		}
+	} else {
+		cout << "Ingreso un numero negativo" << endl;
 	}
 	return 0;
 }
