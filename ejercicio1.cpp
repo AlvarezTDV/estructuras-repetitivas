8db6fab2985dd7e663853141709f9083ac4a42f0
Dante <dalvarezt@unjbg.edu.pe>
2024-06-14 00:07:50 -0500

    Mejora al ejercicio 1 de estructuras repetitivas. Se implemento un verificador para que cuando se ingrese un número negativo se repita y pida de nuevo un número, se va a repetir hasta que se ingrese un número positivo

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -3,15 +3,19 @@ using namespace std;
 
 int main() {
 	int numeros, multiplos;
-	cout << "Ingrese una cantidad (positivos) que luego seran los primeros multiplos de 7: ";
-	cin >> numeros;
-	if ( numeros > 0 ) {
-		for ( int i = 1; i <= numeros; i++ ) {
-			multiplos = i * 7;
-			cout << multiplos << endl;
+	bool verificador = false;
+	do {
+		cout << "Ingrese una cantidad (positivos) que luego seran los primeros multiplos de 7: ";
+		cin >> numeros;
+		if ( numeros > 0 ) {
+			for ( int i = 1; i <= numeros; i++ ) {
+				multiplos = i * 7;
+				cout << multiplos << endl;
+			}
+			verificador = true;
+		} else {
+			cout << "Ingreso un numero negativo" << endl;
 		}
-	} else {
-		cout << "Ingreso un numero negativo" << endl;
-	}
+	} while ( verificador != true );
 	return 0;
 }
