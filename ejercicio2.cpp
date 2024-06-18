0ea0781a1dccf7429a2cd9bbcbd33ffd1cbcd4cf
Dante <dalvarezt@unjbg.edu.pe>
2024-06-17 23:04:54 -0500

    Mejora al ejercicio 2 de estructuras repetitivas. Se mejoro la interfaz, simula ser como un cuadro, se añadío el titulo del programa y un mensaje al terminar el programa

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -4,18 +4,28 @@ using namespace std;
 int main () {
 	int numero, intentos;
 	string verificador;
+	cout << "------------------------------------------------------------" << endl;
+	cout << "Programa que cuenta los numeros positivos hasta uno negativo" << endl;
+	cout << "------------------------------------------------------------" << endl;
 	do {
 		intentos = 1;
-		cout << "Ingresa un numero: ";
+		cout << "Ingresa un numero => ";
 		cin >> numero;
 		while ( numero >= 0 ) {
-			cout << "Ingresa otro numero: ";
+			cout << "Ingresa otro numero => ";
 			cin >> numero;
 			intentos++;
 		}
-		cout << "Ingreso un numero negativo" << endl << "Los intentos fueron: " << intentos << endl;
-		cout << "¿Desea volver a ingresar numeros? (si o no): ";
+		cout << "------------------------------------------------------------" << endl;
+		cout << "Ingreso un numero negativo" << endl;
+		cout << "------------------------------------------------------------" << endl;
+		cout << "Los intentos fueron: " << intentos << endl;
+		cout << "------------------------------------------------------------" << endl;
+		cout << "Desea volver a ingresar numeros (si o no): ";
 		cin >> verificador;
+		cout << "------------------------------------------------------------" << endl;
 	} while ( verificador != "no" );
+	cout << "Gracias por usar el programa" << endl;
+	cout << "------------------------------------------------------------" << endl;
 	return 0;
 }
