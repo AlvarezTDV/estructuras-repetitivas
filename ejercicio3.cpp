653d0c4fa51cde9bdbdd276ea82953c5255bced2
Dante <dalvarezt@unjbg.edu.pe>
2024-06-19 00:09:54 -0500

    Mejora al ejercicio 3 de estructuras repetitivas. Se añadio un do while para repetir en cada ocasión que el usuario ingrese un número negativo. Tambien se mejoró la interfaz y se añadío un título con un mensaje de despedida

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -10,14 +10,27 @@ int sumatoria ( int x ){
 
 int main() {
 	int numeros, suma;
+	bool verificador = false;
 	suma = 0;
-	cout << "Ingrese una cantidad de numeros: ";
-	cin >> numeros;
-	if ( numeros >= 0 ) {
-		suma = sumatoria(numeros);
-		cout << "La suma de los primeros numeros es: " << suma;
-	} else {
-		cout << "Ingreso un numero negativo";
-	}
+	cout << "--------------------------------------------------" << endl;
+	cout << "    Programa para sumar los n primeros numeros    " << endl;
+	cout << "--------------------------------------------------" << endl;
+	do {
+		cout << "Ingrese una cantidad de numeros => ";
+		cin >> numeros;
+		cout << "--------------------------------------------------" << endl;
+		if ( numeros >= 0 ) {
+			suma = sumatoria(numeros);
+			cout << "La suma de los " << numeros << " primeros numeros es => " << suma << endl;
+			cout << "--------------------------------------------------" << endl;
+			verificador = true;
+		} else {
+			cout << "        ERROR: Ingreso un numero negativo         " << endl;
+			cout << "              Vuelva a intentarlo                 " << endl;
+			cout << "--------------------------------------------------" << endl;
+		}
+	} while ( verificador != true );
+	cout << "          Gracias por usar el programa            " << endl;
+	cout << "--------------------------------------------------" << endl;
 	return 0;
 }
